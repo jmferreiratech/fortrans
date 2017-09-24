a6ce884ae07358a9a029fea312cc0cade9247de1
Marcelo Ferreira <joamarcelo@gmail.com>
2017-09-24 20:01:56 -0300

    Fixes assets' folders paths

--- sw.js
+++ sw.js
@@ -1,35 +1,25 @@
-const version = 4;
+const version = 6;
 const currentCacheName = 'v' + version;
 
 const files = [
-    "/",
     "index.html",
     "app.js",
-    "Content/",
-    "Content/Images",
     "Content/Images/nacessivel2.gif",
     "Content/Images/acessivel.gif",
     "Content/Images/ajax-loader2.gif",
     "Content/Images/acessivel.ico",
     "Content/bootstrap.min.css",
-    "Controllers/",
     "Controllers/linHorariosCtrl.js",
-    "fonts/",
     "fonts/glyphicons-halflings-regular.eot?",
     "fonts/glyphicons-halflings-regular.ttf",
     "fonts/glyphicons-halflings-regular.eot",
-    "images/",
     "images/icon-32.png",
     "images/icon-512.png",
     "images/icon-192.png",
-    "Scripts/",
     "Scripts/angular.min.js",
-    "Scripts/i18n/",
     "Scripts/i18n/angular-locale_pt-br.js",
-    "Scripts/angular-ui",
     "Scripts/angular-ui/ui-bootstrap-tpls.min.js",
     "Scripts/angular-ui/ui-bootstrap.min.js",
-    "Scripts/utilitarios/",
     "Scripts/utilitarios/utilitarios.js",
 ];
 
